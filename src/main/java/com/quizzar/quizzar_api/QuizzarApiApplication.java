cc2f2ef04fea9202bbddb1bd9725d8ccb38fb889
PreciousIfeaka <preciousifeaka@gmail.com>
2026-05-16 17:38:18 +0100

    chore: project & dependencies setup

---
+++ src/main/java/com/quizzar/quizzar_api/QuizzarApiApplication.java
@@ -0,0 +1,13 @@
+package com.quizzar.quizzar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quizzar/quizzar_api/QuizzarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quizzar.quizzar_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
